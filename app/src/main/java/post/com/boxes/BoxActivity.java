a1f467d3f54d405dc6d8fbd59a530a3f7a8e753c
Ruby <ruby10387@gmail.com>
2018-11-06 15:47:51 +0800

    box2

---
+++ app/src/main/java/post/com/boxes/BoxActivity.java
@@ -0,0 +1,14 @@
+package post.com.boxes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_box);
+        
+    }
+}
--- app/src/main/java/post/com/boxes/MainActivity.java
+++ app/src/main/java/post/com/boxes/MainActivity.java
@@ -28,12 +28,17 @@ public class MainActivity extends AppCompatActivity {
         float length = Float.parseFloat(l);
         float width = Float.parseFloat(w);
         float height = Float.parseFloat(h);
-        if (length <= 23 && width <= 14 && height <= 13){
-            Intent intent = new Intent(this,ResultActivity.class);
-            startActivity(intent);
-        }else{
-            Intent intent = new Intent(this,Result2Activity.class);
-            startActivity(intent);
-        }
+//        if (length <= 23 && width <= 14 && height <= 13){
+//            Intent intent = new Intent(this,ResultActivity.class);
+//            startActivity(intent);
+//        }else{
+//            Intent intent = new Intent(this,Result2Activity.class);
+//            startActivity(intent);
+//        }
+      Intent intent = new Intent(this,BoxActivity.class);
+      intent.putExtra("length",length);
+      intent.putExtra("width",width);
+      intent.putExtra("height",height);
+      startActivity(intent);
     }
 }
